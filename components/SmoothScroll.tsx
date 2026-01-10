cbaf80e70894ef91b36f851c8203ace39d91d226
omkar-uni <omkar.lonkar1263@gmail.com>
2026-01-10 16:22:19 +0530

    Added components

--- components/SmoothScroll.tsx
+++ components/SmoothScroll.tsx
@@ -7,9 +7,9 @@ export default function SmoothScroll({ children }: { children: ReactNode }) {
     useEffect(() => {
         const lenis = new Lenis({
             duration: 1.2,
-            easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+            easing: (t: number) =>
+                Math.min(1, 1.001 - Math.pow(2, -10 * t)),
             smoothWheel: true,
-            smoothTouch: false,
         });
 
         function raf(time: number) {
